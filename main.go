a0aa994c722ad115e3587874a42f5dff809669ef
daecotr <gkonstantin0908@gmail.com>
2024-12-09 12:42:40 +0200

    Simple counter

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+  "net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
+var counter uint
+
 func main() {
+  counter = 0
+
 	router := gin.Default()
+  router.LoadHTMLGlob("templates/*")
+
+  router.GET("/", getIndex)
+
 	router.Run(":8080")
+}
+
+func getIndex(context *gin.Context) {
+  context.HTML(http.StatusOK, "index.tmpl", gin.H{
+	  "counter": counter,
+  })
+  counter++
 }
