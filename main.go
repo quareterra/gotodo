84c8446c764c63607b34b70603d42756f10b1a7f
daecotr <gkonstantin0908@gmail.com>
2024-12-10 16:12:39 +0200

    Dynamic list (add function)

--- main.go
+++ main.go
@@ -6,36 +6,26 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var message string
-var isLinethroughed bool
+var elementList []string
 
 func main() {
-	message = "initial message"
-
 	router := gin.Default()
 	router.Static("/static", "./static")
 	router.LoadHTMLGlob("templates/*")
 
 	router.GET("/", getIndex)
-	router.POST("/set", setMessage)
-	router.POST("/switch", switchLinethrough)
+	router.POST("/addElement", addElement)
 
 	router.Run(":8080")
 }
 
-func setMessage(context *gin.Context) {
-	message = context.PostForm("message")
+func addElement(context *gin.Context) {
+	elementList = append(elementList, context.PostForm("element"))
 	context.Redirect(http.StatusFound, "/")
 }
 
 func getIndex(context *gin.Context) {
 	context.HTML(http.StatusOK, "index.tmpl", gin.H{
-		"message":     		 	message,
-		"isLinethroughed": 	isLinethroughed,
+		"elementList": elementList,
 	})
-}
-
-func switchLinethrough(context *gin.Context) {
-	isLinethroughed = !isLinethroughed
-	context.Redirect(http.StatusFound, "/")
 }
